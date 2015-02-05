9bc418d7b666702062dd133110eaea757430df1e
Jorge Demetrio Sobieski Teixeira <jorge.demetrio@alldreams.com.br>
2015-02-05 00:25:11 -0200

    Revert "Revert "Criação de tela de busca, home e sitemap""
    This reverts commit 9da6b7f7232a4e2b2d51ec462bb85459127d5003.

---
+++ com_angelgirls/site/views/homepage/tmpl/default.php
@@ -0,0 +1,13 @@
+<script>
+  (function() {
+    var cx = '007002280365302771262:n0131qtlwdq';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
