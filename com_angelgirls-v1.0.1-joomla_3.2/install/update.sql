3bb209fdce6be852b6868bc85d7165063d3ca9f4
Jorge Demetrio Sobieski Teixeira <jorge.demetrio@alldreams.com.br>
2015-10-26 14:46:52 -0200

    Correções de banco

--- com_angelgirls-v1.0.1-joomla_3.2/install/update.sql
+++ com_angelgirls-v1.0.1-joomla_3.2/install/update.sql
@@ -6,3 +6,34 @@ CREATE TABLE `#__angelgirls_vt_visitante` (
 	FOREIGN KEY (`id_usuario`) REFERENCES `#__users` (`id`),
 	PRIMARY KEY(`id_visitante`,`id_usuario`)
 ) ENGINE = InnoDB   DEFAULT CHARSET=utf8;
+
+
+
+
+
+
+
+
+
+
+
+
+ALTER TABLE `#__angelgirls_post` 
+CHANGE COLUMN `token` `token` VARCHAR(150) NOT NULL ,
+ADD UNIQUE INDEX `token_UNIQUE` (`token` ASC);
+
+ALTER TABLE `#__angelgirls_promocao` 
+CHANGE COLUMN `token` `token` VARCHAR(150) NOT NULL ,
+ADD UNIQUE INDEX `token_UNIQUE` (`token` ASC);
+
+ALTER TABLE `#__angelgirls_sessao` 
+ADD UNIQUE INDEX `token_UNIQUE` (`token` ASC);
+
+ALTER TABLE `#__angelgirls_foto_sessao`  
+ADD UNIQUE INDEX `token_UNIQUE` (`token` ASC);
+
+ALTER TABLE `#__angelgirls_foto_album` 
+ADD UNIQUE INDEX `token_UNIQUE` (`token` ASC);
+
+
+
